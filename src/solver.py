421cc70f7db666146aafde5ddf647cd08b5c49a4
ubombar <ufuk@bombar.dev>
2022-04-27 01:18:57 +0300

    Implement capacity and demand

--- src/solver.py
+++ src/solver.py
@@ -1,94 +1,9 @@
+from pprint import pprint
 import sys
+from traceback import print_tb
 import numpy as np
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
-
-def preprocess_matrix(matrix, jobs):
-    '''
-    The distance matrix should only contain the indexes given in jobs. If not,
-    the solver also calculates routes for locations that does not have a job.
-    As an easy fix, I will set the distances from and to these redundant locations
-    to 0.
-
-    I am also specifying the ending location. I also create a virtual location and make
-    the cost of going there zero. This can be done by padding the matrix.
-    '''
-    # Convert the matrix into numpy array
-    matrix = np.array(matrix, dtype=int)
-    
-    # Padd the matrix only bottom and right
-    matrix = np.pad(matrix, 1, constant_values=0)[1:, 1:]
-
-    # Find the redundant indexes and make their cost zero
-    all_locs = {i for i in range(len(matrix))}
-    jobs_locs = {job['location_index'] for job in jobs}
-    redundant_locs = all_locs.difference(jobs_locs)
-
-    for loc_index in redundant_locs:
-        matrix[loc_index, :] = 0
-        matrix[:, loc_index] = 0
-
-    # We are done now we can use our matrix
-    return matrix
-
-
-def solve_vrp(vehicles, jobs, matrix):    
-    # Preprocess the matrix and get the desired version
-    matrix = preprocess_matrix(matrix=matrix, jobs=jobs)
-
-    # Get the number of vehicles and number of locations including the virtual one
-    num_vehicles = len(vehicles)
-    num_locations = len(matrix)
-
-    # Set starting locations of the vehicles
-    starting_location = [v['start_index'] for v in vehicles]
-
-    # Set the ending locations to the virtual location we created that has no cost
-    ending_location = [num_locations - 1 for _ in vehicles]
-
-    # This is for getting the id of the job from its order location
-    location_index_to_job_id = {job['location_index']:job['id'] for job in jobs}
-
-    # Create the routing index manager.
-    manager = pywrapcp.RoutingIndexManager(
-        num_locations, 
-        num_vehicles, 
-        starting_location, 
-        ending_location)
-
-    # Create Routing Model.
-    routing = pywrapcp.RoutingModel(manager)
-    
-    # Create and register a transit callback. This is for calculatiing
-    def distance_callback(from_index, to_index):
-        from_node = manager.IndexToNode(from_index)
-        to_node = manager.IndexToNode(to_index)
-        return matrix[from_node][to_node]
-
-    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-
-    # Add Distance constraint.
-    # Maximum time for any vehicle is set to the sum of the matrix. By this way there won't be a
-    # limitation on time.
-    routing.AddDimension(
-        transit_callback_index,
-        0,
-        int(matrix.sum()), 
-        True,
-        'Time')
-    distance_dimension = routing.GetDimensionOrDie('Time')
-    distance_dimension.SetGlobalSpanCostCoefficient(100)
-
-    # Setting first solution heuristic.
-    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
-
-    # Solve the problem.
-    solution = routing.SolveWithParameters(search_parameters)
-
-    return process_solution(manager, routing, solution, vehicles, location_index_to_job_id)
-
 
 def  process_solution(manager, routing, solution, vehicles, location_index_to_job_id):
     total_delivery_duration = 0
@@ -133,3 +48,120 @@ def  process_solution(manager, routing, solution, vehicles, location_index_to_jo
     return dict(
         total_delivery_duration=total_delivery_duration, 
         routes=routes)
+
+def preprocess_matrix(matrix, jobs):
+    '''
+    The distance matrix should only contain the indexes given in jobs. If not,
+    the solver also calculates routes for locations that does not have a job.
+    As an easy fix, I will set the distances from and to these redundant locations
+    to 0.
+
+    I am also specifying the ending location. I also create a virtual location and make
+    the cost of going there zero. This can be done by padding the matrix.
+    '''
+    # Convert the matrix into numpy array
+    matrix = np.array(matrix, dtype=int)
+    
+    # Padd the matrix only bottom and right
+    matrix = np.pad(matrix, 1, constant_values=0)[1:, 1:]
+
+    # Find the redundant indexes and make their cost zero
+    all_locs = {i for i in range(len(matrix))}
+    jobs_locs = {job['location_index'] for job in jobs}
+    redundant_locs = all_locs.difference(jobs_locs)
+
+    for loc_index in redundant_locs:
+        matrix[loc_index, :] = 0
+        matrix[:, loc_index] = 0
+
+    # We are done now we can use our matrix
+    return matrix, redundant_locs
+
+def create_demands(num_locations, jobs, redundant_locs, location_index_to_job_id):
+    '''
+    This unction creates the demand, but also nullifies the redundant location demands.
+    '''
+
+    demands = [0 for _ in range(num_locations)]
+
+    for i, job in enumerate(jobs):
+        demands[job['location_index']] = job['delivery'][0]
+
+    return demands
+
+def solve_vrp(vehicles, jobs, matrix):
+    # This is for getting the id of the job from its order location
+    location_index_to_job_id = {job['location_index']:job['id'] for job in jobs}
+
+    # Preprocess the matrix and get the desired version
+    matrix, redundant_locs = preprocess_matrix(matrix, jobs)
+
+    # Get the number of vehicles and number of locations including the virtual one
+    num_vehicles = len(vehicles)
+    num_locations = len(matrix)
+
+    # Amount of carboys that will be delivered to this job
+    demands = create_demands(num_locations, jobs, redundant_locs, location_index_to_job_id)
+
+    # The vehicle capacities
+    capacities = [v['capacity'][0] for v in vehicles]
+
+    # Set starting locations of the vehicles
+    starting_location = [v['start_index'] for v in vehicles]
+
+    # Set the ending locations to the virtual location we created that has no cost
+    ending_location = [num_locations - 1 for _ in vehicles]
+
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(
+        num_locations, 
+        num_vehicles, 
+        starting_location, 
+        ending_location)
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+    
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return matrix[from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Add Distance constraint.
+    # Maximum time for any vehicle is set to the sum of the matrix. By this way there won't be a
+    # limitation on time.
+    routing.AddDimension(
+        transit_callback_index,
+        0,
+        int(matrix.sum()), 
+        True,
+        'Time')
+    distance_dimension = routing.GetDimensionOrDie('Time')
+    distance_dimension.SetGlobalSpanCostCoefficient(100)
+
+    # Create and register a demand callback.
+    def demand_callback(from_index):
+        from_node = manager.IndexToNode(from_index)
+        return demands[from_node]
+    
+    # Register the demand callback
+    demand_callback_index = routing.RegisterUnaryTransitCallback(demand_callback)
+    routing.AddDimensionWithVehicleCapacity(
+        demand_callback_index,
+        0, 
+        capacities, 
+        True, 
+        'Capacity')
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    return process_solution(manager, routing, solution, vehicles, location_index_to_job_id)
